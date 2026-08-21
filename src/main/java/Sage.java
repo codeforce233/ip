09432e1c1c18e98b09f8bb759a9dbdd79b1ec08c
codeforce233 <e1136893@u.nus.edu>
2026-08-21 09:21:19 +0800

    Implement Level-2

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -16,6 +16,9 @@ public class Sage {
         System.out.println(line);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             System.out.println(line);
@@ -24,7 +27,20 @@ public class Sage {
                 System.out.println(line);
                 break;
             }
-            System.out.println(input);
+
+            if ("list".equals(input)) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+                continue;
+            }
+
+            if (taskCount < tasks.length) {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            }
             System.out.println(line);
         }
     }
