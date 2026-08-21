641466fa37004436e360d21ed08b35f0c9043aa4
codeforce233 <e1136893@u.nus.edu>
2026-08-21 08:59:51 +0800

    Rename Duke and update documentation

---
+++ src/main/java/Sage.java
@@ -0,0 +1,18 @@
+public class Sage {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String banner = "  ____                       \n"
+                + " / ___|  __ _  __ _  ___     \n"
+                + " \\___ \\ / _` |/ _` |/ _ \\    \n"
+                + "  ___) | (_| | (_| |  __/    \n"
+                + " |____/ \\__,_|\\__, |\\___|    \n"
+                + "              |___/          \n";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Sage.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
